84c00f52c02facb944d2867b4494f51eff723038
Daniel M. Sánchez <dmsanchez86@outlook.com>
2015-08-17 00:30:51 +0000

    Update class index

--- waves.js
+++ waves.js
@@ -1,38 +1,23 @@
 window.onload = function(){
-    // alert('Page Load Correctly!');
-    var logo = document.querySelector('.logo');
+    var cellphone = document.querySelector('.cellphone');
     var body = document.querySelector('.body');
     var btn_switch = document.querySelector('.on_off');
     
     btn_switch.onclick = function(){
         var state = this.getAttribute('state');
-        console.log(state);
+      
         if(state == 'off'){
             this.className = 'on_off active';
             this.setAttribute('state','on');
-            logo.className  = 'logo hover';
+            cellphone.className  = 'cellphone hover';
         }else{
             this.className = 'on_off';
             this.setAttribute('state','off');
-            logo.className  = 'logo';
+            cellphone.className  = 'cellphone';
         }
     };
     
-    // logo.onmouseover = function(){
-    //     this.className = "logo hover";
-    //     setTimeout(function(){
-    //         body.style.perspectiveOrigin = "center";
-    //     },2500);
-    // };
-    // logo.onmouseout = function(){
-    //     this.className = "logo";
-    //     setTimeout(function(){
-    //         body.style.perspectiveOrigin = "center";
-    //     },2500);
-    // };
-    
     document.onclick = function(evt){
-        // alert('Click in document');
         var position_X = evt.clientX - 50;
         var position_Y = evt.clientY - 50;
         
@@ -51,6 +36,5 @@ window.onload = function(){
             },600);
         },10);
         
-        console.log(evt);
     };
 };
